95ffea2fdbdc2ab336c2a797f1f3472d47d15f0c
Ewelina <ewelinazawol@gmail.com>
2023-05-03 15:15:44 +0100

    MongoDB connected

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-import express, { Request, Response } from "express";
-import { sampleProducts } from "./data";
+import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import { productRouter } from "./routers/productRouter";
+import { seedRouter } from "./routers/seedRouter";
+// import expressListRoutes from "express-list-routes";
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/leafyLane";
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("Error connecting to MongoDB", error.message);
+  });
 
 const app = express();
 app.use(cors({ credentials: true, origin: ["http://localhost:5173"] }));
 
-app.get("/api/products", (req: Request, res: Response) => {
-  res.json(sampleProducts);
-});
-
-app.get("/api/products/:slug", (req: Request, res: Response) => {
-  const product = sampleProducts.find((p) => p.slug === req.params.slug);
-  res.json(product);
-});
-
+app.use("/api/products", productRouter);
+app.use("/api/seed", seedRouter);
+// expressListRoutes(app);
 const PORT = 4000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
