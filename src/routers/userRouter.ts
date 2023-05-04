c02d2e5d16de8ddafcdf774b46b602b3d727a608
Ewelina <ewelinazawol@gmail.com>
2023-05-04 16:46:13 +0100

    SignUp endpoint

--- src/routers/userRouter.ts
+++ src/routers/userRouter.ts
@@ -25,3 +25,27 @@ userRouter.post(
     res.status(401).json({ message: "Invalid email or password" });
   })
 );
+
+userRouter.post(
+  "/signup",
+  asyncHandler(async (req: Request, res: Response) => {
+    const exist = await UserModel.findOne({ email: req.body.email });
+    if (exist) {
+      res.status(400).json({ message: "User with this email already exists" });
+      return;
+    }
+    const user = await UserModel.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+    return;
+  })
+);
