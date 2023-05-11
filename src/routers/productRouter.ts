487017469c0d1689aa61aea7f74384bfcaf82493
Ewelina <ewelinazawol@gmail.com>
2023-05-11 21:25:47 +0100

    Create Product

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,6 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import asyncHandler from "express-async-handler";
 import { ProductModel } from "../models/productModel";
+import { isAuth } from "../utils";
+import multer from "multer";
+const upload = multer({ dest: "uploads/" });
 
 export const productRouter = express.Router();
 
@@ -32,5 +35,27 @@ productRouter.get(
     } else {
       res.status(404).json({ message: "Product not found" });
     }
+  })
+);
+
+productRouter.post(
+  "/",
+  isAuth,
+  asyncHandler(async (req: Request, res: Response) => {
+    const createdProduct = await ProductModel.create({
+      name: req.body.name,
+      slug: req.body.slug,
+      price: req.body.price,
+      image: req.body.image,
+      brand: req.body.brand,
+      category: req.body.category,
+      countInStock: req.body.countInStock,
+      description: req.body.description,
+      rating: req.body.rating,
+      numReviews: req.body.numReviews,
+    });
+    res
+      .status(201)
+      .json({ message: "Product Created", product: createdProduct });
   })
 );
