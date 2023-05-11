5bd827836a0d15a494290716e44790fe51b84f28
Ewelina <ewelinazawol@gmail.com>
2023-05-11 18:38:28 +0100

    Fetch Product By Id

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -11,6 +11,17 @@ productRouter.get(
     res.json(products);
   })
 );
+productRouter.get(
+  "/product/:id",
+  asyncHandler(async (req, res) => {
+    const product = await ProductModel.findById(req.params.id);
+    if (product) {
+      res.json(product);
+    } else {
+      res.status(404).json({ message: "Product not found" });
+    }
+  })
+);
 
 productRouter.get(
   "/:slug",
